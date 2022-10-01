faab7820143ea15ce73b71d366355cb99cc75150
fouzankp <muhammedfouzankp@gmail.com>
2022-10-01 13:24:06 +0530

    Removed table from db, created models and generated table and added data to table from terminal

--- MainPage/models.py
+++ MainPage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Rooms(models.Model):
+    Roomno = models.IntegerField(primary_key=True)
+    TYPES = [('S', 'Single'),('D','Double')]
+    RoomType = models.CharField(choices=TYPES,max_length=10)
+    STYPES = [('C','CLEANED'),('N','NOTCLEANED'),('O','OCCUPIED')]
+    Status = models.CharField(default='C',choices=STYPES,max_length=10)
+    Checkin = models.DateTimeField(null=True)
+    Reserved = models.BooleanField(default=False)
+
+
